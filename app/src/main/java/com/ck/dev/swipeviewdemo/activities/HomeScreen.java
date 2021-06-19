f57e935bb1bacfb99137b81bc20b5bbce4ae2cdb
Jashanpreet Singh <jashan.singh.work@icloud.com>
2021-06-19 22:14:26 +0530

    Linked views with backend.

--- app/src/main/java/com/ck/dev/swipeviewdemo/activities/HomeScreen.java
+++ app/src/main/java/com/ck/dev/swipeviewdemo/activities/HomeScreen.java
@@ -3,14 +3,30 @@ package com.ck.dev.swipeviewdemo.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import com.ck.dev.swipeviewdemo.R;
 
 public class HomeScreen extends AppCompatActivity {
 
+    private LinearLayout swipeDetectorView;
+    private LinearLayout moveView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_home_screen);
+        initView();
+    }
+
+    private void initView() {
+        swipeDetectorView = this.findViewById(R.id.swipe_detector_view);
+        moveView          = this.findViewById(R.id.move_view);
+
+        onClick();
+    }
+
+    private void onClick() {
+
     }
 }
