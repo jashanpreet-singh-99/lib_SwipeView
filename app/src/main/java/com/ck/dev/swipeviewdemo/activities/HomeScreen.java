ef20ab9cf4f7f393d63c6fa239f033e011dc823e
Jashanpreet Singh <jashan.singh.work@icloud.com>
2021-06-19 22:27:15 +0530

    Initial swipe movement added.

--- app/src/main/java/com/ck/dev/swipeviewdemo/activities/HomeScreen.java
+++ app/src/main/java/com/ck/dev/swipeviewdemo/activities/HomeScreen.java
@@ -3,9 +3,13 @@ package com.ck.dev.swipeviewdemo.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.LinearLayout;
 
 import com.ck.dev.swipeviewdemo.R;
+import com.ck.dev.swipeviewdemo.interfaces.OnSwipeEvent;
+import com.ck.dev.swipeviewdemo.utils.SwipeDetector;
+import com.ck.dev.swipeviewdemo.utils.SwipeType;
 
 public class HomeScreen extends AppCompatActivity {
 
@@ -27,6 +31,30 @@ public class HomeScreen extends AppCompatActivity {
     }
 
     private void onClick() {
+        new SwipeDetector(swipeDetectorView).setOnSwipeListener(new OnSwipeEvent() {
+            @Override
+            public void swipeEventDetected(View v, SwipeType swipeType) {
+                switch(swipeType) {
+                    case BOTTOM_TO_TOP:
+                        break;
+                    case TOP_TO_BOTTOM:
+                        break;
+                }
+            }
 
+            @Override
+            public void swipeMovementValue(float valueX, float valueY) {
+                if (valueX <= 0)
+                    return;
+
+                float change = swipeDetectorView.getHeight() - valueY;
+                moveView.setTranslationY(change);
+            }
+
+            @Override
+            public void tapEventDetected(View v, float valueX, float valueY) {
+
+            }
+        });
     }
 }
