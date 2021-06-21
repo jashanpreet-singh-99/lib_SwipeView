154be28b37ab524a6185f6c78bc9453ebf6505bf
Jashanpreet Singh <jashan.singh.work@icloud.com>
2021-06-21 22:23:59 +0530

    removed unused imports.

--- app/src/main/java/com/ck/dev/swipeviewdemo/utils/SwipeDetector.java
+++ app/src/main/java/com/ck/dev/swipeviewdemo/utils/SwipeDetector.java
@@ -6,8 +6,6 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewConfiguration;
 
-import androidx.core.view.ViewConfigurationCompat;
-
 import com.ck.dev.swipeviewdemo.interfaces.OnSwipeEvent;
 
 public class SwipeDetector implements View.OnTouchListener {
